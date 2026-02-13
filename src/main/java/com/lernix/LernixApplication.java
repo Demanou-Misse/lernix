f2f90823d3d4e180020dbc66447a7985d310dd15
Demanou Misse Jr <missedemanou@gmail.com>
2026-02-13 05:26:33 +0100

    chore: initialize professional project structure

---
+++ src/main/java/com/lernix/LernixApplication.java
@@ -0,0 +1,13 @@
+package com.lernix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LernixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LernixApplication.class, args);
+	}
+
+}
